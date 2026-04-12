c43585fd1e8806820324663188acbaa8ee45cf06
Sebastian Pawliński <90553150+sebastianp265@users.noreply.github.com>
2026-04-12 13:53:01 +0200

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int r = Arrays.stream(piles).max().orElse(1);
+        int l = 1;
+        while (l < r) {
+            int p = l + (r - l) / 2;
+            var hoursSpentEatingWithPRate = hoursSpentEating(piles, p);
+            if (hoursSpentEatingWithPRate <= h) {
+                r = p;
+            } else {
+                l = p + 1;
+            }
+        }
+
+        return l;
+    }
+
+    private int hoursSpentEating(int[] piles, int bananasPerHour) {
+        return Arrays.stream(piles)
+            .map(p -> p / bananasPerHour + (p % bananasPerHour != 0 ? 1 : 0))
+            .sum();
+    }
+}
