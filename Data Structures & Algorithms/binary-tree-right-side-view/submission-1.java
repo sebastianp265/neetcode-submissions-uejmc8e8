be813aa6ff845a3d749f725dea1890802fdd8326
Sebastian Pawliński <90553150+sebastianp265@users.noreply.github.com>
2026-04-12 15:13:16 +0200

    Add: binary-tree-right-side-view - submission-1

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        var result = new ArrayList<Integer>();
+        dfs(root, result, 0);
+
+        return result;
+    }
+
+    private void dfs(TreeNode node, List<Integer> result, int depth) {
+        if (node == null) {
+            return;
+        }
+        if (result.size() == depth) {
+            result.add(node.val);
+        }
+
+        dfs(node.right, result, depth + 1);
+        dfs(node.left, result, depth + 1);
+    }
+}
