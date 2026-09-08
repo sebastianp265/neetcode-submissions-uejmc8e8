4507596b8b632f9ed8b6f23a839b9ce6276de93f
Sebastian Pawliński <90553150+sebastianp265@users.noreply.github.com>
2026-09-08 16:12:05 +0200

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.java
@@ -0,0 +1,48 @@
+class Solution {
+    public void reorderList(ListNode firstPart) {
+        var n = countElements(firstPart);
+        ListNode lastElementOfFirstPart = firstPart;
+        // TODO: verify even lengths
+        for (int i = 1; i < n / 2 + n % 2; i++) {
+            lastElementOfFirstPart = lastElementOfFirstPart.next;
+        }
+        var secondPart = lastElementOfFirstPart.next;
+        lastElementOfFirstPart.next = null;
+        secondPart = reverseList(secondPart);
+
+        while(secondPart != null) {
+            var firstPartNext = firstPart.next;
+            var secondPartNext = secondPart.next;
+
+            firstPart.next = secondPart;
+            secondPart.next = firstPartNext;
+
+            secondPart = secondPartNext;
+            firstPart = firstPartNext;
+        }
+
+    }
+
+    ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        var current = head;
+        while (current != null) {
+            var next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+
+    private int countElements(ListNode listNode) {
+        int i = 0;
+        while (listNode != null) {
+            i++;
+            listNode = listNode.next;
+        }
+        return i;
+    }
+
+}
