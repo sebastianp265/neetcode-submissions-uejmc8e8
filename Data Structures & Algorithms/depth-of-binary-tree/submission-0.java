e55c76ee4b42611634fb5cc95e19f9595bac6051
Sebastian Pawliński <90553150+sebastianp265@users.noreply.github.com>
2026-09-08 20:01:49 +0200

    Add: depth-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-0.java
@@ -0,0 +1,11 @@
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        return maxDepth(root, 0);
+    }
+
+    private int maxDepth(TreeNode node, int depth) {
+        if (node == null) return depth;
+        return Math.max(maxDepth(node.left, depth + 1), maxDepth(node.right, depth + 1));
+    }
+}
